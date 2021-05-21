e2fe1731a24715f4e322b9b3c937fe2ef98c9c3a
오범수 <skfk0135@gmail.com>
2021-05-21 16:22:24 +0900

    feat: export zoomkr_module constant

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,1 @@
-const hello = 'this is hello';
-
-function oneline() {
-  console.log('oneline function');
-}
-function multiline() {
-  console.log('multiline function');
-}
+export { ZoomkrModule, ZOOMKR_MODULE } from './zoomkr.module';
---
+++ lib/zoomkr.module.ts
@@ -0,0 +1,20 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import * as Zoom from 'zoomkr';
+
+export const ZOOMKR_MODULE = 'ZOOMKR_MODULE';
+
+@Module({})
+export class ZoomkrModule {
+  static forRoot(): DynamicModule {
+    const zoomkrProvider = {
+      provide: ZOOMKR_MODULE,
+      useValue: Zoom,
+    };
+
+    return {
+      module: ZoomkrModule,
+      providers: [zoomkrProvider],
+      exports: [zoomkrProvider],
+    };
+  }
+}
